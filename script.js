af3cac1e94a6d8e4b70888cb4326f772e45b45a9
Hwiwon-Seo <sshnrg2@naver.com>
2023-04-30 21:16:35 +0900

    1.1
    key

--- script.js
+++
@@ -1,8 +0,0 @@
-fetch('config.json')
-  .then(res => res.json())
-  .then(config => {
-    const apikey = config.key;
-
-    const element = document.getElementById('api-key');
-    element.innerText = apikey;
-});
